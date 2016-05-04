f3bf4a4456b02514f05ccb7b50a63a50a6353602
danrnascimento <danrnascimento@gmail.com>
2016-05-04 00:46:03 -0300

    Funções Produto

--- cadastro/Cadastro.java
+++ cadastro/Cadastro.java
@@ -14,7 +14,7 @@ import java.util.*;
 public class Cadastro {
 	
 	public static Map<String, dados.Cliente> clientes = new HashMap<String, dados.Cliente>();
-	public static Map<String, String> produtos = new HashMap<String, String>();
+	public static Map<Integer, dados.Produto> produtos = new HashMap<Integer, dados.Produto>();
 	public static Map<String, String> vendas = new HashMap<String, String>();
 
 	/**
@@ -89,8 +89,77 @@ public class Cadastro {
 		
 	}
 	
+	/**
+	 * incluir produto a lista de produtos
+	 * @author Daniel Nascimento
+	 * @param produto Produto - produto a ser incluido
+	 */
 	
+	public static void incluirProduto(Produto produto){
+		produtos.put(produto.getCodigo(), produto);
+	}
 	
+	/**
+	 * remover produto da lista de produtos
+	 * @author Daniel Nascimeno
+	 * @param produto Produto - produto a ser removido
+	 */
+	
+	public static void removerProduto(Produto produto){
+		produtos.remove(produto.getCodigo());
+	}
+	
+	/**
+	 * procurar produto na lista de produtos pelo codigo
+	 * @author Daniel Nascimento
+	 * @param cod Int - codigo a ser pesquisado
+	 * @throws SisVendasException
+	 */
+	
+	public static Produto procurarProdutoCod(int cod) throws SisVendasException{
+		
+		if (produtos.containsKey(cod)){
+			return produtos.get(cod);
+		}else{
+			throw new SisVendasException("Não encontrado");
+		}
+		
+	}
+	
+	/**
+	 * procurar pelo nome do produto e exibir em ordem alfabetica
+	 * @author Daniel Nascimento
+	 * @param nome String - nome do produto a ser procurado
+	 * @throws SisVendaException
+	 */
+	
+	public static ArrayList<Produto> procurarProdutoNome(String nome) throws SisVendasException{
+		
+		ArrayList<Produto> listaAux = new ArrayList<Produto>();
+		
+		for(Produto auxiliar : produtos.values()){
+			if(auxiliar.getNome().contains(nome)){
+				listaAux.add(auxiliar);
+			}
+		}
+		
+		if(listaAux.isEmpty()){
+			throw new SisVendasException("Não encontrdo");
+		}else{
+			Collections.sort(listaAux, new Comparator<Produto>() {
+				
+				@Override
+				public int compare(Produto auxiliar1, Produto auxiliar2) {
+					// TODO Auto-generated method stub
+					return auxiliar1.getNome().compareTo(auxiliar2.getNome());
+				}
+				
+			});
+			
+			return listaAux;
+		}
+		
+	}
 	
 	
 }
